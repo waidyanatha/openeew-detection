945d4812a69a3258846290add305d28f8c643aa9
Egidio Caprino <egidio.caprino@gmail.com>
2020-07-24 20:00:19 +0200

    fix(detection): detection scripts takes into account multiple sensors

--- scripts/detection.py
+++ scripts/detection.py
@@ -10,7 +10,7 @@ from trigger import accel_value
 
 
 # initializing empty variables
-inbox = []
+inbox = {}
 
 # trigger parameters
 long_window = 11
@@ -28,74 +28,74 @@ def authenticate(client):
 
 def set_time(times, sr, N):
     """
-    times = time stamps by fifo in each payload 
-    sr = sample rate 
+    times = time stamps by fifo in each payload
+    sr = sample rate
     N = number of data samples
     """
-    
+
     # number of fifos
     nfifo = len(times)
-    
-    # create an empty variable to allocate the 
+
+    # create an empty variable to allocate the
     diff = []
-    
+
     if nfifo > 1:
-        # Loop over the times of each fifo 
+        # Loop over the times of each fifo
         for i, item in enumerate(times[0:-1]):
             diff.append(times[i+1] - item)
-        
+
         # Estimate the delta t per data tupple
         delta_t = numpy.mean(diff) / (N/nfifo)
-    else: 
+    else:
         delta_t = 1 / sr
         times.append(times[0] + N*delta_t)
-    
+
     # Defines the time for each tupple
     t = numpy.arange(times[0] - ((N/nfifo))*delta_t, times[-1]+delta_t, delta_t).tolist()
-    
+
     return t
 
 
 def parser_json(payload):
     '''
     Parser payload from mqtt
-    Format json 
+    Format json
     Returns:
         device_id
         cloud_t
-        traces 
-        sr 
+        traces
+        sr
     where:
     traces =  {"t" : numpy.array(t), "x" : numpy.array(x), "y" : numpy.array(y), "z" : numpy.array(z)}
-    
+
     '''
     payload = json.loads(payload)
     device_id = payload["device_id"]
     cloud_t = payload["cloud_t"]
-    
+
     # jsonl from esp32 sensors
-    if len(payload) == 3: 
+    if len(payload) == 3:
         _x = []
         _y = []
         _z = []
         _t = []
-        
+
         for i, item in enumerate(payload["traces"]):
             _x.append(item["x"])
             _y.append(item["y"])
             _z.append(item["z"])
             _t.append(item["t"])
             sr = item["sr"]
-        
+
         x = [item for sublist in _x for item in sublist]
         y = [item for sublist in _y for item in sublist]
         z = [item for sublist in _z for item in sublist]
-        
+
         # Set times per each data tupple (x, y and z)
         t = set_time(_t, sr, len(x))
-        
+
         traces = {"t" : t, "x" : x, "y" : y, "z" : z}
-    
+
     # jsonl from rp sensors
     elif len(payload) == 8:
 
@@ -105,9 +105,9 @@ def parser_json(payload):
         z = payload["z"]
         _t = [payload["device_t"]]
         t = set_time(_t, sr, len(x))
-    
+
         traces = {"t" : t, "x" : x, "y" : y, "z" : z}
-    
+
     return device_id, cloud_t, traces, sr
 
 
@@ -122,72 +122,75 @@ def on_publish(host, port, topic, data_out):
 
 
 def on_message(client, userdata, msg):
-    
+
     m_decode = str(msg.payload.decode("utf-8","ignore"))
     m_in = json.loads(m_decode)
-    
-    # appending msg 
-    # Missing check if the msgs are from different sensors
-    inbox.append(m_in)
-    
-    # When the msgs are more or equal than the long window
-    if len(inbox) >= long_window:
-        # Empty variables for the last 10 seconds
-        _x = []
-        _y = []
-        _z = []
-        _t = []
-        # Looping over the inbox elements
-        for i, item in enumerate(inbox):
-            device_id, cloud_t, traces, sr = parser_json(item)
-            #print(i, device_id)
 
-            _x.extend(traces["x"])
-            _y.extend(traces["y"])
-            _z.extend(traces["z"])
-            _t.extend(traces["t"])
-        
-        print("Device id:", device_id)
+    # appending msg
+    device_id = parser_json(m_in)
+    if device_id not in inbox:
+        inbox[device_id] = []
+    inbox[device_id].append(m_in)
 
-        # -------------- MOVING WINDOW -----------------------------
-        # Select the last seconds and rename
-        n = len(traces["x"])
-        x = _x[-n*long_window:]
-        y = _y[-n*long_window:]
-        z = _z[-n*long_window:]
-        t = _t[-n*long_window:]
-        #print("Longitude:", len(x), len(y), len(z), len(t))
-        
-        # -------------- TRIGGER SECTION -----------------------------
-        # STA / LTA algorithm
-        x_sta_lta = sta_lta(numpy.array(x), short_window*n, long_window*n)
-        y_sta_lta = sta_lta(numpy.array(y), short_window*n, long_window*n)
-        z_sta_lta = sta_lta(numpy.array(z), short_window*n, long_window*n)
+    for device_id, device_inbox in inbox.items():
+        # When the msgs are more or equal than the long window
+        if len(device_inbox) >= long_window:
+            # Empty variables for the last 10 seconds
+            _x = []
+            _y = []
+            _z = []
+            _t = []
+            # Looping over the inbox elements
+            for i, item in enumerate(device_inbox):
+                device_id, cloud_t, traces, sr = parser_json(item)
+                #print(i, device_id)
 
-        
-        # Estimating trigger times given a trigger level
-        ttimes_x = trigger_time(x_sta_lta, numpy.array(t), trigger_level)
-        ttimes_y = trigger_time(y_sta_lta, numpy.array(t), trigger_level)
-        ttimes_z = trigger_time(z_sta_lta, numpy.array(t), trigger_level)
+                _x.extend(traces["x"])
+                _y.extend(traces["y"])
+                _z.extend(traces["z"])
+                _t.extend(traces["t"])
 
-        # -------------- CHARACTERIZATION SECTION -----------------------------
-        nttimes = len(ttimes_x) + len(ttimes_y) + len(ttimes_z) 
-        if nttimes > 0:
-            print("------------> Trigger of %s components. Sensor %s. Time %s" % (nttimes, device_id, cloud_t))
-            accel = accel_value(numpy.array(x), numpy.array(y), numpy.array(z))
-            pga = numpy.round(numpy.max(accel),3)
-            print("Acceleration: ", pga)
+            print("Device id:", device_id)
 
-            # --------------PUBLISH SECTION -----------------------------
-            data_out = {"device_id" : numpy.str(device_id),"time" : numpy.str(cloud_t), "pga" : numpy.str(pga)}
-            # topic
-            topic = "/pga-trigger"
-            host = "localhost"  
-            port = 1883  
-            client.on_publish = on_publish(host, port, topic, numpy.str(data_out))
+            # -------------- MOVING WINDOW -----------------------------
+            # Select the last seconds and rename
+            n = len(traces["x"])
+            x = _x[-n*long_window:]
+            y = _y[-n*long_window:]
+            z = _z[-n*long_window:]
+            t = _t[-n*long_window:]
+            #print("Longitude:", len(x), len(y), len(z), len(t))
+
+            # -------------- TRIGGER SECTION -----------------------------
+            # STA / LTA algorithm
+            x_sta_lta = sta_lta(numpy.array(x), short_window*n, long_window*n)
+            y_sta_lta = sta_lta(numpy.array(y), short_window*n, long_window*n)
+            z_sta_lta = sta_lta(numpy.array(z), short_window*n, long_window*n)
 
 
-# --------------MQTT SECTION ----------------------------- 
+            # Estimating trigger times given a trigger level
+            ttimes_x = trigger_time(x_sta_lta, numpy.array(t), trigger_level)
+            ttimes_y = trigger_time(y_sta_lta, numpy.array(t), trigger_level)
+            ttimes_z = trigger_time(z_sta_lta, numpy.array(t), trigger_level)
+
+            # -------------- CHARACTERIZATION SECTION -----------------------------
+            nttimes = len(ttimes_x) + len(ttimes_y) + len(ttimes_z)
+            if nttimes > 0:
+                print("------------> Trigger of %s components. Sensor %s. Time %s" % (nttimes, device_id, cloud_t))
+                accel = accel_value(numpy.array(x), numpy.array(y), numpy.array(z))
+                pga = numpy.round(numpy.max(accel),3)
+                print("Acceleration: ", pga)
+
+                # --------------PUBLISH SECTION -----------------------------
+                data_out = {"device_id" : numpy.str(device_id),"time" : numpy.str(cloud_t), "pga" : numpy.str(pga)}
+                # topic
+                topic = "/pga-trigger"
+                host = "localhost"
+                port = 1883
+                client.on_publish = on_publish(host, port, topic, numpy.str(data_out))
+
+
+# --------------MQTT SECTION -----------------------------
 client = authenticate(mqtt.Client())
 client.on_message = on_message
 client.on_connect = on_connect
@@ -197,12 +200,13 @@ client.loop_start()
 
 print('init')
 
-# Continue loop 
-while True: 
-    if len(inbox) <= long_window:
-        continue
-    else: 
-        inbox = inbox[-long_window:]
+# Continue loop
+while True:
+    for device_id in inbox:
+        if len(inbox[device_id]) <= long_window:
+            continue
+        else:
+            inbox[device_id] = inbox[device_id][-long_window:]
 
 
 #client.disconnect()
