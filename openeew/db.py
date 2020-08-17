6f3def8b295a58bf3a72a432492327b5eda77c38
Egidio Caprino <egidio.caprino@gmail.com>
2020-08-17 21:57:50 +0200

    chore(devices): add function for persisting devies

---
+++ openeew/db.py
@@ -0,0 +1,21 @@
+from psycopg2 import connect
+
+_connection = None
+
+
+def _get_connection():
+    global _connection
+    if _connection is None:
+        _connection = connect(
+            user='detector',
+            host='127.0.0.1',
+            port=5432,
+            database='detector'
+        )
+    return _connection
+
+
+def execute_statement(statement, parameters):
+    connection = _get_connection()
+    cursor = connection.cursor()
+    cursor.execute(statement, parameters)
---
+++ test/test_devices.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+
+class SaveDeviceTest(TestCase):
+    @patch('openeew.db.execute_statement')
+    def test_device_is_persisted(self, mock_execute_statement):
+        from openeew.devices import save_device
+        input = {
+            'device_id': '2351n2x55',
+            'latitude': 19.35,
+            'longitude': -99.14,
+            'firmware_version': 1.2,
+            'device_type': 'esp32',
+            'time_entered': 1597685615,
+        }
+        save_device(input)
+        mock_execute_statement.assert_called_once()
